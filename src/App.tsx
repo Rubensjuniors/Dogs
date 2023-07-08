6ae7d08872544b183d54d63604e2c62f3d4fafbd
rubens.marques <rubens.marques@hotmart.com>
2023-07-08 17:16:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-slate-600 text-red-800">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
